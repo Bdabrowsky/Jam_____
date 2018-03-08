559b7ac06875eb4b0e5e6a638dd1dc93c6ca13de
Bdabrowswki <bartosz.dabrowski.org@gmail.com>
2018-03-08 19:31:02 +0100

    0

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-before_create :confirmation_token
   has_secure_password
+  
   validates :email, uniqueness: true, presence: true
-
-
 end
--- db/migrate/20180308182442_app.rb
+++ db/migrate/20180308182442_app.rb
@@ -1,7 +1,7 @@
 class App < ActiveRecord::Migration[5.1]
   def change
-  	create_table :users do |t|
-      t.string :name
-      t.digest :password
+  	create_table "users", force: :cascade do |t|
+      t.string "name"
+      t.string "password_digest"
   end
 end
