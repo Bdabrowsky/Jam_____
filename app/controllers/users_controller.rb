bab694726743036ffdf05e68e77b9503b8a960c2
Bdabrowswki <bartosz.dabrowski.org@gmail.com>
2018-03-08 18:32:26 +0100

    0.0.0.1
    2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,18 +23,6 @@ class UsersController < ApplicationController
 
   end
 
-    def confirm_email
-    user = User.find_by_confirm_token(params[:id])
-    if user
-      user.email_activate
-      flash[:success] = "Welcome to the Sample App! Your email has been confirmed.
-      Please sign in to continue."
-      redirect_to signin_url
-    else
-      flash[:error] = "Sorry. User does not exist"
-      redirect_to root_url
-    end
-    end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
