9fa0ce0e279b8101483b59b34b67f04dfeaba3f0
Bdabrowswki <bartosz.dabrowski.org@gmail.com>
2018-03-08 18:41:36 +0100

    0.0.0.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
   before_action :reset_session
 
   def new
-    @user = User.new
+    @user = User.create
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
 
     if @user.save
       session[:user_id] = @user.id
