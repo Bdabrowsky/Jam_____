2ea4c46561128b89c697a2204fe92f8cd971ef36
Bdabrowswki <bartosz.dabrowski.org@gmail.com>
2018-03-09 19:20:22 +0100

    0.0.0.11

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'dashboard#show'
+  root  'dashboard#show'
 
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
-  
+
   resources :sessions, only: [:create]
   resources :users, only: [:new, :create]
 
